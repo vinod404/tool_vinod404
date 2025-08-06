22f5e064224c38a5a153efe0c3ab3c159bf1616a
vinod <vinod.aleti@moodle.com>
2025-08-06 18:26:30 +0530

    Second release with main page

--- index.php
+++ index.php
@@ -15,14 +15,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for tool_vinod404.
+ * Basic information displayed on the plugin's index page.
  *
  * @package   tool_vinod404
- * @copyright 2025 Vinod Kumar Aleti <vinod.aleti@moodle.com>
+ * @copyright 2025, Vinod Kumar Aleti <vinod.aleti@moodle.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') || die();
-
-$string['pluginname'] = 'My first Moodle plugin';
-$string['vinod404'] = 'Vinod Learning Plugin';
-$string['helloworld'] = 'Hello, World!';
+require_once(__DIR__ . '/../../../config.php');
+$url = new \moodle_url('/admin/tool/vinod404/index.php');
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url($url);
+$PAGE->set_pagelayout('report');
+$PAGE->set_title(get_string('vinod404', 'tool_vinod404'));
+$PAGE->set_heading(get_string('pluginname', 'tool_vinod404'));
+echo $OUTPUT->header();
+echo get_string('helloworld', 'tool_vinod404');
+echo $OUTPUT->footer();
